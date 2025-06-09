0bd286762efcf93381e62f37ed6211f6cfe9859d
Hanaha7 <hannaoktavia55@gmail.com>
2025-06-09 13:15:07 +0700

    Revisi

--- Login.cs
+++ Login.cs
@@ -16,5 +16,10 @@ namespace HoloSouls_PDKR_NSPD
         {
             InitializeComponent();
         }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
