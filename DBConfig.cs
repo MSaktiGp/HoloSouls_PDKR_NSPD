22ef2ecb2d750bf4f84ccb77d703bf96201d27f7
Hanaha7 <hannaoktavia55@gmail.com>
2025-06-09 13:01:36 +0700

    tambah desain

---
+++ DBConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoloSouls_PDKR_NSPD
+{
+    internal class DBConfig
+    {
+    }
+}
