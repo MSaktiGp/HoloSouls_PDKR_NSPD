29c405e151f70ed0ffe86ca36a7c4d3fd6367b9d
Hanaha7 <hannaoktavia55@gmail.com>
2025-06-09 13:22:40 +0700

    Revisi lagi mungkin

--- Login.Designer.cs
+++ Login.Designer.cs
@@ -114,7 +114,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImage = global::HoloSouls_PDKR_NSPD.Properties.Resources.line_md_download;
             this.ClientSize = new System.Drawing.Size(525, 418);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
