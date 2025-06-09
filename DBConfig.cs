85b5b9357795f70925dd377ff281f22de6b99f41
Dina Putri Chairani <dinaputrichairani28@gmail.com>
2025-06-09 13:59:29 +0700

    tambah DbConfig

--- DBConfig.cs
+++ DBConfig.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
 
 namespace HoloSouls_PDKR_NSPD
 {
     internal class DBConfig
     {
+        public static string dbServer = "localhost";
+        public static string dbUser = "root";
+        public static string dbName = "nasipadang_db";
+        public static string dbPass = "";
+
+        public static string ConnStr => $"server={dbServer};user={dbUser};database={dbName};password={dbPass};";
     }
 }
