85063724bd8e93cd157d5e37626ff164acb18d70
Melanifitri27 <melanifitriastria458@gmail.com>
2025-06-13 22:21:12 +0700

    revisi desain imel

--- FormAdminMenu.Designer.cs
+++ FormAdminMenu.Designer.cs
@@ -225,7 +225,7 @@
             this.Controls.Add(this.label2NamaMenu);
             this.Controls.Add(this.dataGridView1);
             this.Name = "FormAdminMenu";
-            this.Text = "FormAdminMenu";
+            this.Text = "Menu";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
